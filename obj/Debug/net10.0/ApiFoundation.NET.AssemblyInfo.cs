ddf283d9e1bc25874bb3e26d0df4337a92396e9f
Cesar Ogando <cesarogando0112@gmail.com>
2026-02-27 23:13:47 -0400

    chore: add gitignore and remove build artifacts

--- obj/Debug/net10.0/ApiFoundation.NET.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiFoundation.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiFoundation.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213625d83d9f013648af7820a94e52a2d161244e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiFoundation.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiFoundation.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
